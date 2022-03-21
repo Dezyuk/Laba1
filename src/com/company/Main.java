9a8b5333d407e6191d4a9736278937f096ed4a5c
Dezyuk <romanyuk.groz@gmail.com>
2022-03-21 09:12:42 +0200

    Task 41

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        task41();
+    }
+    private  static  void task41(){
+        int n = (int) (Math.random() * 10 + 1);
+        int y=1;
+        for (int i = 2; i <= 2*n; i+=2) {
+            y*=i;
+        }
+        System.out.println("n=" + n + "\ny=" + y);
     }
 }
