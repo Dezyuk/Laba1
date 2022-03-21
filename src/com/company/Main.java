0ffb7f161788b86c606c88dbfdec48a2ec3e51b6
Dezyuk <romanyuk.groz@gmail.com>
2022-03-21 11:12:47 +0200

    Task 116

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,10 @@ public class Main {
         //task41();
         //task66();
         //task91();
-        task116();
+        //task116();
+        task141();
     }
+    //Дано натуральное число n. Вычислить у = 2 · 4 · 6 ... (2n).
     private  static  void task41(){
         int n = (int) (Math.random() * 10 + 1);
         int y=1;
@@ -19,12 +21,14 @@ public class Main {
         }
         System.out.println("n=" + n + "\ny=" + y);
     }
+    //Составить программу для расчёта степени n вещественного числа а (n — натуральное число).
     private  static  void task66(){
         float a = ((float) ((int) (Math.random() * 100 + 1))/10);
         int n = (int) (Math.random() * 10 + 1);
         float result= (float) Math.pow(a,n);
         System.out.println("Число а = "+ a + "\nСтепень n = "+ n +"\nРезультат ="+ result);
     }
+    //Даны натуральное число n и вещественные числа а1, a2, …, аn. Определить среднее арифметическое этих чисел.
     private  static  void  task91(){
         int n = (int) (Math.random() * 10 + 1);
         float sum=0;
@@ -36,7 +40,23 @@ public class Main {
         System.out.println(n);
         System.out.println("Среднее арефметическое = "+ sum/n);
     }
+    //Составить программу, которая печатает таблицу умножения и сложения натуральных чисел в десятичной системе счисления.
     private static void task116(){
+        for (int i = 1; i <= 10; i++) {
+            System.out.println();
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(j+"*"+i+"="+i*j+"\t\t");
+            }
+        }
+        System.out.println("\n\n\n");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println();
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(j+"+"+i+"="+(i+j)+"\t\t");
+            }
+        }
+    }
+    private static void task141(){
 
     }
 }
